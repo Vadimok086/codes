b4d68d2841703ff6ae3c1d1ef19c152c1c0bb940
Vadimok086 <109691254+Vadimok086@users.noreply.github.com>
2023-12-20 12:12:44 +0200

    Update Startup.cs

--- lab3/Startup.cs
+++ lab3/Startup.cs
@@ -4,3 +4,20 @@ public void ConfigureServices(IServiceCollection services)
 
     services.AddSingleton<AppState>();
 }
+{
+
+    services.AddSession(options =>
+    {
+        options.IdleTimeout = TimeSpan.FromMinutes(30);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+    });
+
+}
+
+public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+
+    app.UseSession();
+
+}
